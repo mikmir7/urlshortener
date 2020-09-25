4adf02bb2bf7d2edeaf4200cc6ac7424a165a742
unknown <mikaelm7777777@gmail.com>
2020-09-25 17:27:14 +0200

    added mongodb mongoose bodyparser shortid to package.json , updated server and index page

--- server.js
+++ server.js
@@ -1,29 +1,86 @@
 'use strict';
 
+var database_uri = 'mongodb+srv://mikmir7:Aninotzri20@cluster1.dxthi.mongodb.net/Cluster1?retryWrites=true&w=majority'
+
+
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 var cors = require('cors');
-
 var app = express();
-
+var shortid = require('shortid');
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.DB_URI);
+mongoose.connect(database_uri,
+   { useNewUrlParser: true, useUnifiedTopology: true }); 
 
-app.use(cors());
+
+ // enable CORS so that your API is remotely testable by FCC
+app.use(cors({optionsSuccessStatus: 200}));
+
+//  https:// expressjs.com/en/starter/static-files.html
+ app.use(express.static('public'));
+ 
+// express.js basic routing
+
+ app.get('/', function (req, res) {
+ res.send(__dirname + '/views/index.html');
+ });
+
+// schema and model 
+var ShortUrl = mongoose.model('ShortUrl', new mongoose.Schema ({
+  short_url: String,
+  original_url: String,
+  suffix: String
+}));
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+// parse application/json
+app.use(bodyParser.json())
+
+app.post("/api/shorturl/new", function(req, res) {
+  var client_requested_url = req.body.url
+  var suffix = shortid.generate();
+  var newShortUrl = suffix
+
+  var newUrl = new ShortUrl({
+    short_url: __dirname + "/api/shorturl" + suffix,
+    original_url: client_requested_url,
+    suffix: suffix
+  })
+
+
+  newUrl.save(function(err, doc){
+    if (err) return console.error(err);
+    res.json({
+      "saved": true,
+      "short_url": newUrl.short_url,
+      "original_url": newUrl.original_url,
+      "suffix": newUrl.suffix
+
+    });
+
+  });
 });
+
+app.get("/api/shorturl/:suffix", (req, res) => {
+  var userGeneratedSuffix = req.params.suffix;
+  ShortUrl.find({suffix: userGeneratedSuffix}).then(foundUrls => {
+    var urlForRedirect = foundUrls[0];
+    res.redirect(urlForRedirect.original_url);
+  });
+});
+
+
 
   
 // your first API endpoint... 
